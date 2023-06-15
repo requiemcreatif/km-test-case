ddbd4217798a04d31d5a9db662d06d2084f8e681
Alain Mani <requiemcreatif@gmail.com>
2023-06-15 21:31:13 +0200

    Budget formated

--- components/Campaign.tsx
+++ components/Campaign.tsx
@@ -1,38 +1,61 @@
-import React from 'react'
+import React from "react";
+import { AiFillCheckCircle, AiOutlineClose } from "react-icons/ai";
+
 
 interface CampaignProps {
-    campaign: {
-        id: number,
-        name: string,
-        startDate: string,
-        endDate: string,
-        Budget: number
-    }
+  campaign: {
+    id: number;
+    name: string;
+    startDate: string;
+    endDate: string;
+    Budget: number;
+  };
 }
-
 
 const Campaign: React.FC<CampaignProps> = ({ campaign }) => {
-    const startDate = new Date(campaign.startDate);
-    const endDate = new Date(campaign.endDate);
-    const today = new Date();
-    const budgetFormatted = campaign.Budget.toLocaleString();
+  const startDate = new Date(campaign.startDate);
+  const endDate = new Date(campaign.endDate);
+  const today = new Date();
+  const budgetFormatted = campaign.Budget.toFixed(2).toLocaleString();
 
-    // Format date strings
-    const startDateFormatted = `${(startDate.getMonth()+1).toString().padStart(2, '0')}/${startDate.getDate().toString().padStart(2, '0')}/${startDate.getFullYear()}`;
-    const endDateFormatted = `${(endDate.getMonth()+1).toString().padStart(2, '0')}/${endDate.getDate().toString().padStart(2, '0')}/${endDate.getFullYear()}`;
 
-    const isActive = today >= startDate && today <= endDate;
-    //const isActive = true;
+  // Format date strings
+  const startDateFormatted = `${(startDate.getMonth() + 1).toString().padStart(2, "0")}/${startDate
+    .getDate()
+    .toString()
+    .padStart(2, "0")}/${startDate.getFullYear()}`;
+  const endDateFormatted = `${(endDate.getMonth() + 1).toString().padStart(2, "0")}/${endDate
+    .getDate()
+    .toString()
+    .padStart(2, "0")}/${endDate.getFullYear()}`;
 
-    return (
-        <tr className={isActive ? "bg-green-200" : "bg-red-200"}>
-            <td className="border px-4 py-2">{campaign.name}</td>
-            <td className="border px-4 py-2">{isActive ? "Active" : "Inactive"}</td>
-            <td className="border px-4 py-2">{startDateFormatted}</td>
-            <td className="border px-4 py-2">{endDateFormatted}</td>   
-            <td className="border px-4 py-2">${budgetFormatted}</td>
-        </tr>
-    );
-}
+  const isActive = today >= startDate && today <= endDate;
+  //const isActive = true;
+
+  return (
+    <tr className={isActive ? "bg-green-200" : "bg-red-200"}>
+      <td className='border px-4 py-2'>{campaign.name}</td>
+      <td className='border px-4 py-2'>
+        {isActive ? (
+          <div className="flex items-center gap-2">
+            <AiFillCheckCircle className='text-green-700' />
+            <span>Active </span>
+            
+          </div>
+        ) : (
+          <div className="flex items-center gap-2">
+            <AiOutlineClose className='text-red-500' />
+            <span>Inactive </span>
+            
+          </div>
+        )}
+      </td>
+
+      <td className='border px-4 py-2'>{startDateFormatted}</td>
+      <td className='border px-4 py-2'>{endDateFormatted}</td>
+      <td className='border px-4 py-2'>${budgetFormatted}</td>
+    </tr>
+  );
+};
 
 export default Campaign;
